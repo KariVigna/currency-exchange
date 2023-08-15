fd4ea12bb6b6ce3cba8906a00cecb6e35e43af4d
Kari Vigna <karivigna24@gmail.com>
2023-08-14 18:01:49 -0700

    class created for currency exchange

--- src/currency.js
+++ src/currency.js
@@ -1,5 +1,6 @@
-function getConversion() {
-fetch(`https://v6.exchangerate-api.com/v6/${process.env.API-KEY}/latest/USD`)
+export default class CurrencyExchange {
+  static getConversion() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API-KEY}/latest/USD`)
       .then(response => onRecieve_API_Response(response))      
       .catch(function(error) {
         return error;
@@ -7,10 +8,12 @@ fetch(`https://v6.exchangerate-api.com/v6/${process.env.API-KEY}/latest/USD`)
 
       function onRecieve_API_Response(response) {
         if (!response.ok) {
-            const errorMessage = `${response.status} ${response.statusText}`;
-            throw new Error(errorMessage);
-            } else {
-                return response.json();
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
         }
-      }}
+      }
+  }
+}
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './currency.js'
 
 window.addEventListener("load", function() {
     console.log("page loaded js working!");
@@ -10,9 +11,9 @@ function handleFormSubmission(event) {
     event.preventDefault();
     const usdAmount = document.querySelector('#usdInput').value;
     const selectedCurrency = document.querySelectorAll("input[name='curr']:checked");
+    // document.querySelector("result").innerText = ${response.conversion_rates.[selectedCurrency]}
 }
 window.addEventListener("load", function(){
     document.querySelector("#conversion").addEventListener("submit", handleFormSubmission)
 })
 
-// document.querySelector("result").innerText = ${response.conversion_rates.[selectedCurrency]}
