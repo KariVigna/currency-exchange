c70e40f6cc192e42b8fb0f32cdf265bd09a51d6e
Kari Vigna <karivigna24@gmail.com>
2023-08-15 17:32:20 -0700

    further progress on js

--- src/currency.js
+++ src/currency.js
@@ -1,6 +1,7 @@
 export default class CurrencyExchange {
   static getConversion() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API-KEY}/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    
       .then(response => onRecieve_API_Response(response))      
       .catch(function(error) {
         return error;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,34 @@ window.addEventListener("load", function() {
     console.log("page loaded js working!");
 });
 
+
+function getCurrency(amt, selectedCurr) {
+    CurrencyExchange.getConversion().then((res) => {
+        if (res.result != "success") {
+            printError(res);
+        } else {
+            printElements(res, amt, selectedCurr);
+        }
+    })
+}
+
+function printElements(res) {
+    console.log("Success",[res, amt, selectedCurr])
+}
+
+function printError(res) {
+    console.log("Error",res)
+}
+
+
 function handleFormSubmission(event) {
     event.preventDefault();
     const usdAmount = document.querySelector('#usdInput').value;
     const selectedCurrency = document.querySelectorAll("input[name='curr']:checked");
-    // document.querySelector("result").innerText = ${response.conversion_rates.[selectedCurrency]}
+    // document.querySelector("result").innerText = ${response.conversion_rates[selectedCurrency]
+    getCurrency()
 }
+
 window.addEventListener("load", function(){
     document.querySelector("#conversion").addEventListener("submit", handleFormSubmission)
 })
