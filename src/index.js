d07466ab4b55cf486f2b160b94766f11074df6c1
Kari Vigna <karivigna24@gmail.com>
2023-08-17 18:54:34 -0700

    wip, some updated index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyExchange from './currency.js'
+import CurrencyExchange from './currency.js';
 
 window.addEventListener("load", function() {
-    document.getElementById("conversion").addEventListener("submit", handleFormSubmission)
-});
+    document.getElementById("conversion").addEventListener("submit", handleFormSubmission);
 
-
-function getCurrency(usdAmount) {
-    CurrencyExchange.getCurrency(usdAmount).then((response) => {
-        if (!response.ok) {
-            printError(response);
+function getCurrency(usdAmount, targetCode) {
+    CurrencyExchange.getCurrency(usdAmount, targetCode).then((response) => {
+        if (response.data === 200) {
+            printElements(response, usdAmount, targetCode);
         } else {
-           printElements(response);
+           printError(response);
         }
-    })
+    });
 }
+function handleFormSubmission(e) {
+    e.preventDefault();
+    const usdAmount = document.getElementById('usdInput').value;
+    const targetCode = document.querySelector("input[name='curr']:checked").value;
+    // document.getElementById("usdInput").value = null;
+    getCurrency(usdAmount, targetCode);
+    console.log("form submitted!");
+    }
 
-function printElements(response) {
-    document.querySelector("result").innerText = null;
+function printElements(response, usdAmount, targetCode) {
+    // document.querySelector("result").innerText = null;
     const resultBox = document.getElementById("resultBox");
     resultBox.classList.remove("hidden");
-    document.getElementById("result").innerText = `Your result is: ${response.conversion_result}`
+    console.log("success");
+    console.log(response, usdAmount, targetCode);
+    document.getElementById("result").innerText = `Your result is: ${response.conversion_result}`;
 }
 
 function printError(error) {
     // document.querySelector("result").innerText = null;
     const resultBox = document.getElementById("resultBox");
     resultBox.classList.remove("hidden");
-    document.getElementById("result").innerText = `There was an error processing your data: ${error}`
+    console.log("error")
+    console.log(response)
+    document.getElementById("result").innerText = `There was an error processing your data: ${error}`;
 }
 
-function handleFormSubmission(e) {
-    e.preventDefault();
-    const usdAmount = document.getElementById('usdInput').value;
-    const targetCode = document.querySelectorAll("input[name='curr']:checked");
-    // document.getElementById("usdInput").value = null;
-    getCurrency(usdAmount, targetCode);
-    console.log("form submitted!")
-    }
-
+});
 
